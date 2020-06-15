7e63836968c5c2c49ec4dcc2bbaf355ca5326ab8
lcm <2020520946@qq.com>
2020-06-15 11:49:39 +0800

    1、项目结构目录初始化 2、网站图片

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,22 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+  },
+  data() {
+    return {
+      
+    };
+  },
+  mounted(){
   }
-}
+};
 </script>
 
 <style>
---
+++ vue.config.js
@@ -0,0 +1,18 @@
+//nodeJs的语法规范，就是使用module.exports ，而不是 export default
+//暴露出一个object对象
+module.exports = {
+    //devServer名称是固定的，会默认的加载devServer中的配置表
+    devServer:{
+        host:'localhost',//主机
+        port:8080,//端口
+        proxy:{//代理
+            '/api':{//拦截访问 /activity 的时候
+                target:'https://www.imooc.com',//代理到http://www.imooc.com
+                changeOrigin:true,//是否要将主机头的源点更改为目标url地址
+                pathRewrite:{//转发地址
+                    '/api':''
+                }
+            }
+        }
+    }
+}
